b8e2afad929181efaff80995a505f11b3ffae456
freelancer <californiafreelancer7@gmail.com>
2023-05-14 06:02:31 -0400

    init project laravel + vue + inertia + Tailwind

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import TwtterLayout from '../Layouts/TwitterLayout.vue'
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <TwtterLayout>Hello</TwtterLayout>
+</template>
+
