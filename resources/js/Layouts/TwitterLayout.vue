adb5e2030dfe389760e5ddc3b7ec01cf2c645304
freelancer <californiafreelancer7@gmail.com>
2023-05-16 12:33:56 -0400

    add side-bar

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -15,6 +15,11 @@ import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
 import MenuItem from '../Components/MenuItem.vue'
 
 const showingNavigationDropdown = ref(false);
+
+let file = ref('')
+let randImg = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}/100`)
+
+
 </script>
 
 <template>
@@ -63,8 +68,81 @@ const showingNavigationDropdown = ref(false);
 
             </div>
 
-            <div class="lg:block hidden lg:w-4/12 h-screen border-l border-gray-800 pl-4">
+            <!--            sideBar section         -->
+            <div class="lg:block hidden  bg-black lg:w-4/12 h-screen border-l border-gray-800 pl-4">
+                <!--            search         -->
+                <div class="w-full p-1 mt-2 px-4 lg:flex items-center rounded-full hidden bg-[#212327]">
+                    <Magnify fillColor="#5e5c5c" :size="25"/>
+                    <input
+                        class="
+                            appearance-none
+                            w-full
+                            border-0
+                            py-2
+                            bg-[#212327]
+                            text-gray-100
+                            placeholder-gray-500
+                            leading-tight
+                            focus:ring-0
+                        "
+                        type="text"
+                        placeholder="Search Twitter"
+                    >
+                </div>
 
+                <!--                What's happening            -->
+                <div class="w-full mt-4 rounded-lg lg:block hidden bg-[#212327]">
+                    <div class="w-full p-4 text-white font-extrabold mb-6 text-[20px]">What's happening</div>
+
+                    <div class="h-[80px] hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+                        <div class="flex p-3 justify-between h-[80px] py-3">
+                            <div>
+                                <div class="text-[14px] text-gray-400">NHL Last night</div>
+                                <div class="w-full text-white font-extrabold mb-6 text-[17px]">Kraken at Stars</div>
+                            </div>
+                            <img class="rounded-xl"  :src="randImg" alt="" />
+                        </div>
+                    </div>
+
+                    <div class="hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+                        <div class="flex p-3 justify-between">
+                            <div>
+                                <div class="text-[14px] text-gray-400">Technology · Trending</div>
+                                <div class="w-full text-white font-extrabold text-[17px]">#WhatsApp</div>
+                                <div class="text-[14px] text-gray-400">5,923 Tweets</div>
+                            </div>
+                            <DotsHorizontal fillColor="#5e5c5c"/>
+                        </div>
+                    </div>
+
+                    <div class="hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+                        <div class="flex p-3 justify-between">
+                            <div>
+                                <div class="text-[14px] text-gray-400">Sports · Trending</div>
+                                <div class="w-full text-white font-extrabold text-[17px]">Poro</div>
+                                <div class="text-[14px] text-gray-400">12,432 Tweets</div>
+                            </div>
+                            <DotsHorizontal fillColor="#5e5c5c"/>
+                        </div>
+                    </div>
+
+                    <div class="hover:bg-gray-800 cursor-pointer transition duration-200 ease-in-out">
+                        <div class="flex p-3 justify-between">
+                            <div>
+                                <div class="text-[14px] text-gray-400">Politics · Trending</div>
+                                <div class="w-full text-white font-extrabold text-[17px]">Russia</div>
+                                <div class="text-[14px] text-gray-400">98,745 Tweets</div>
+                            </div>
+                            <DotsHorizontal fillColor="#5e5c5c"/>
+                        </div>
+                    </div>
+
+
+
+                    <div class="hover:bg-gray-800 rounded-b-xl cursor-pointer transition duration-200 ease-in-out">
+                        <div class="text-blue-500 p-4">Show more</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
