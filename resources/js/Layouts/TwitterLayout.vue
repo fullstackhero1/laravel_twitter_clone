c6ff9f4544d5629240a54cebf415ee6c46ee5d3c
freelancer <californiafreelancer7@gmail.com>
2023-05-16 09:33:19 -0400

    add Twit section

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -20,12 +20,51 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div class="fixed w-full">
         <div class="max-w-[1400px] flex mx-auto">
+            <!--            MENU         -->
             <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:px-4 lg:mx-auto bg-black">
+                <div class="p-2 px-3 mb-4">
+                    <Twitter fillColor="#FFFFFF" :size="37" />
+                </div>
                 <MenuItem iconString="Home"/>
                 <MenuItem iconString="Explore"/>
                 <MenuItem iconString="Notifications"/>
                 <MenuItem iconString="Messages"/>
                 <MenuItem iconString="Profile"/>
+            </div>
+
+
+            <!--            Twit section         -->
+            <div class="lg:w-6/12 w-11/12 border-x border-gray-800 relative">
+                <div class=" bg-black backdrop-blur-md z-10 absolute w-full">
+                    <div class="border-gray-800 border-b w-full">
+                        <div class="w-full text-white text-[22px] font-extrabold p-4">
+                            Home
+                        </div>
+                        <div class="flex">
+                            <div
+                                class="flex items-center justify-center w-full h-[60px] text-white text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
+                                <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
+                                    <div class="my-auto mt-4">For you </div>
+                                </div>
+                            </div>
+                            <div
+                                class="w-full h-[60px] text-gray-500 text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
+                                <div class="text-center"> Following </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="absolute top-0 z-0 h-full w-full overflow-auto scrollbar-hide">
+                    <div class="mt-[126px]"></div>
+                    <slot />
+                    <div class="pb-4"></div>
+                </div>
+
+            </div>
+
+            <div class="lg:block hidden lg:w-4/12 h-screen border-l border-gray-800 pl-4">
+
             </div>
         </div>
     </div>
